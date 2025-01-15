2a03b8533bdaadc572c97b59a4af3abb8a7758d4
mdwensma <mdwensma@ncsu.edu>
2025-01-14 22:36:48 -0500

    2nd sol

--- MergeStringsAlternatively/Solution.java
+++ MergeStringsAlternatively/Solution.java
@@ -31,4 +31,22 @@ class Solution {
         return ret;
 
     }
+
+    public String mergeAlternatelyOne(String word1, String word2) {
+
+        StringBuilder merged = new StringBuilder();
+        int maxLength = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                merged.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                merged.append(word2.charAt(i));
+            }
+        }
+
+        return merged.toString();
+
+    }
 }
